8f66fe95dc7ee36f91e07e25cf85ac001cd46838
rintov <rinto0503@gmail.com>
2024-09-05 17:03:21 +0700

    Removed swagger for api gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,27 +1,13 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './app/app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Api-Gateway APIs')
-    .setDescription('Document api-gateway APIs')
-    .setVersion('1.0')
-    .addTag('v1.0')
-    .build();
-    
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
-
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-
   const port = process.env.PORT || 3000;
   await app.listen(port);
-
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 bootstrap();
