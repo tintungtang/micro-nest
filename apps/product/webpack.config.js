347ccde9f0ac476baadca93424fe3917be0dcc43
tinthai <sss@sadsad>
2024-10-03 19:11:27 +0700

    refactor(format): Re-format product service code

--- apps/product/webpack.config.js
+++ apps/product/webpack.config.js
@@ -1,23 +1,22 @@
-
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join } = require('path');
 
 module.exports = {
-  output: {
-    path: join(__dirname, '../../dist/apps/product'),
-    filename: 'main.js',
-    clean: true
-  },
-  plugins: [
-    new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ["./src/assets"],
-      optimization: false,
-      outputHashing: 'none',
-    })
-  ],
+    output: {
+        path: join(__dirname, '../../dist/apps/product'),
+        filename: 'main.js',
+        clean: true
+    },
+    plugins: [
+        new NxAppWebpackPlugin({
+            target: 'node',
+            compiler: 'tsc',
+            main: './src/main.ts',
+            tsConfig: './tsconfig.app.json',
+            assets: ['./src/assets'],
+            optimization: false,
+            outputHashing: 'none'
+        })
+    ]
 };
 
