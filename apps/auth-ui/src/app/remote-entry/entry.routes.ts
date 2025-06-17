e4c406506461114d9875b6ac582347f3c29b4872
Tin Thai <107792718+tintungtang@users.noreply.github.com>
2025-06-17 16:00:08 +0700

    feat(storefront): add dashboard page with officer guard

--- apps/auth-ui/src/app/remote-entry/entry.routes.ts
+++ apps/auth-ui/src/app/remote-entry/entry.routes.ts
@@ -1,6 +1,8 @@
 import { Route } from '@angular/router';
 import { RemoteEntryComponent } from './entry.component';
+import { LoginComponent } from '../pages/login/login.component';
 
 export const remoteRoutes: Route[] = [
   { path: '', component: RemoteEntryComponent },
+  { path: 'login', component: LoginComponent },
 ];
