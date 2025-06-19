a79c57bd60bcdca38075f30c13fe428c513c2a69
Tin Thai <107792718+tintungtang@users.noreply.github.com>
2025-06-19 18:56:46 +0700

    feat(cart): expose cart component

--- apps/cart/module-federation.config.ts
+++ apps/cart/module-federation.config.ts
@@ -5,6 +5,7 @@ const config: ModuleFederationConfig = {
 
     exposes: {
         './Module': './src/remote-entry.ts',
+        './CartComponent': './src/app/components/cart.compoent.tsx',
     },
 };
 
