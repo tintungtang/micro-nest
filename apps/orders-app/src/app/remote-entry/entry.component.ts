87c173a16efc4af14fba199fa7d0d48f55422dd2
duongminhhieu <duongminhhieu2082002@gmail.com>
2025-06-24 23:06:37 +0700

    feat(orders-app): update entry component to use order list

--- apps/orders-app/src/app/remote-entry/entry.component.ts
+++ apps/orders-app/src/app/remote-entry/entry.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { OrderListComponent } from '../components/order-list.component';
 
 @Component({
     standalone: true,
-    imports: [CommonModule, NxWelcomeComponent],
-    selector: 'app-orders-app-entry',
-    template: `<app-nx-welcome></app-nx-welcome>`,
+    imports: [CommonModule, OrderListComponent],
+    selector: 'mfe-order-entry',
+    template: `<app-order-list></app-order-list>`,
 })
-export class RemoteEntryComponent {}
+export class RemoteEntryComponent { }
