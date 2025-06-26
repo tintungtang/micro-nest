200fccacee94c27a8a28eb2cee92bb793c0a5662
duongminhhieu <duongminhhieu2082002@gmail.com>
2025-06-26 21:23:33 +0700

    feat(orders-app): add OrderButton component and integrate into OrderList component

--- apps/orders-app/module-federation.config.ts
+++ apps/orders-app/module-federation.config.ts
@@ -4,6 +4,7 @@ const config: ModuleFederationConfig = {
     name: 'orders-app',
     exposes: {
         './Routes': 'apps/orders-app/src/app/remote-entry/entry.routes.ts',
+        './OrderButton': 'apps/orders-app/src/app/components/order-button.component.ts',
     },
 };
 
